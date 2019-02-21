4dbde1a4024a9abac7ee9b4dd4887cd3d78f66b9
rbracco <robertbracco1@gmail.com>
2019-02-21 18:38:41 -0500

    .

--- app/static/client.js
+++ app/static/client.js
@@ -3,7 +3,8 @@ var el = x => document.getElementById(x);
 function showPicker(inputId) { el('file-input').click(); }
 
 function showPicked(input) {
-    el('upload-label').innerHTML = input.files[0].name;
+    // el('upload-label').innerHTML = input.files[0].name;
+    el('upload-label').innerHTML = "helloWorld";
     var reader = new FileReader();
     reader.onload = function (e) {
         el('image-picked').src = e.target.result;
